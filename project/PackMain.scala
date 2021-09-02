c37787cf6af3303a2b34e26223bef49786e6fb6a
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-09-02 14:05:56 +0200

    implement version of OutsourcedComputationRemoteBlock with dynamically placed remote block

--- project/PackMain.scala
+++ project/PackMain.scala
@@ -1,10 +1,13 @@
 object PackMain {
   val paths = Map(
-    "client-reactive" -> "examples.initial.outsourcedcomputation.reactive.Client",
-    "powerful-server-reactive" -> "examples.initial.outsourcedcomputation.reactive.PowerfulServer",
-    "weak-server-reactive" -> "examples.initial.outsourcedcomputation.reactive.WeakServer",
-    "client-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.Client",
-    "powerful-server-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.PowerfulServer",
-    "weak-server-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.WeakServer"
+    "initial-client-reactive" -> "examples.initial.outsourcedcomputation.reactive.Client",
+    "initial-powerful-server-reactive" -> "examples.initial.outsourcedcomputation.reactive.PowerfulServer",
+    "initial-weak-server-reactive" -> "examples.initial.outsourcedcomputation.reactive.WeakServer",
+    "initial-client-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.Client",
+    "initial-powerful-server-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.PowerfulServer",
+    "initial-weak-server-remoteblock" -> "examples.initial.outsourcedcomputation.remoteblock.WeakServer",
+    "dynamic-client-remoteblock" -> "examples.dynamic.outsourcedcomputation.remoteblock.Client",
+    "dynamic-powerful-server-remoteblock" -> "examples.dynamic.outsourcedcomputation.remoteblock.PowerfulServer",
+    "dynamic-weak-server-remoteblock" -> "examples.dynamic.outsourcedcomputation.remoteblock.WeakServer",
   )
 }
---
+++ src/main/scala/examples/dynamic/outsourcedcomputation/remoteblock/OutsourcedComputationRemoteBlock.scala
@@ -0,0 +1,81 @@
+package examples.dynamic.outsourcedcomputation.remoteblock
+
+import loci._
+import loci.communicator.tcp._
+import loci.transmitter.rescala._
+import loci.serializer.upickle._
+import rescala.default._
+
+import scala.concurrent.Await
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
+@multitier object OutsourcedComputationRemoteBlock {
+
+  @peer type Server
+
+  @peer type Client <: Server { type Tie <: Single[PowerfulServer] with Single[WeakServer] }
+  @peer type PowerfulServer <: Server { type Tie <: Single[Client] }
+  @peer type WeakServer <: Server { type Tie <: Single[Client] }
+
+  val inputNumber: Evt[Int] on Client = on[Client] { implicit! => Evt[Int] }
+
+  def compute(value: Int):  Int on Server = (
+    on[Server] { implicit! =>
+      throw new NotImplementedError
+    }
+    ) and (
+    on[PowerfulServer] { implicit! =>
+      println("Doing some powerful GPU computation stuff but really quick...")
+      value + 1
+    }
+    ) and (
+    on[WeakServer] { implicit! =>
+      println("Compute on shitty slow CPU...")
+      value + 1
+    }
+    )
+
+  implicit def selectServer: Local[Remote[Server]] on Client = on[Client] { implicit! =>
+    val servers = remote[Server].connected.now
+    servers((new Random).nextInt(servers.size))
+  }
+
+  val outputNumber: Event[Future[Int]] on Client = on[Client] { implicit! =>
+    inputNumber.map { value =>
+      onAny[Server].run.capture(value) {implicit! =>
+        compute(value)
+      }.asLocal
+    }
+  }
+
+  def main() = on[Client] {
+    outputNumber observe {
+      output =>
+        println(Await.result(output, 5.seconds))
+    }
+    for (line <- scala.io.Source.stdin.getLines)
+      inputNumber.fire(line.toInt)
+  }
+
+}
+
+object Client extends App {
+  multitier start new Instance[OutsourcedComputationRemoteBlock.Client](
+    connect[OutsourcedComputationRemoteBlock.PowerfulServer] { TCP("localhost", 40001) } and
+      connect[OutsourcedComputationRemoteBlock.WeakServer] { TCP("localhost", 40002) }
+  )
+}
+
+object PowerfulServer extends App {
+  multitier start new Instance[OutsourcedComputationRemoteBlock.PowerfulServer](
+    listen[OutsourcedComputationRemoteBlock.Client] { TCP(40001) }
+  )
+}
+
+object WeakServer extends App {
+  multitier start new Instance[OutsourcedComputationRemoteBlock.WeakServer](
+    listen[OutsourcedComputationRemoteBlock.Client] { TCP(40002) }
+  )
+}
